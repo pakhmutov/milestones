048c7101174ba1424c700d0a4297b936f0644541
pahmutov <pahmutov@f5.com.ru>
2026-01-16 17:12:09 +0300

    feat: update styles and typography, and configure pnpm workspace

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import svgr from "vite-plugin-svgr";
 import { resolve } from 'path'
 
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr({
+      svgrOptions: { exportType: 'default' },
+      include: '**/*.svg',
+  })],
   resolve: {
     alias: {
       "@": resolve(__dirname, "./src"),
